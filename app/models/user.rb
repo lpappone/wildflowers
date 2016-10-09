efc98df421adbd1db088f7676a67e93e744db99c
Lauren Pappone <lauren.pappone@optimizely.com>
2016-10-09 15:54:33 -0700

    add user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates(:firstName, presence: true, length: { maximum: 50 })
+  validates(:lastName, presence: true, length: { maximum: 50 })
+  # From https://www.railstutorial.org/book/modeling_users#cha-6_footnote-ref-11
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates(:email, presence: true, length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false})
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,65 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @user = User.new(firstName: 'example', lastName: 'exshample', email: 'user@gmail.com')
+  end
+
+  test "user should be valid" do
+    assert @user.valid?
+  end
+
+  test "firstname should be present" do
+    @user.firstName = " "
+    assert_not @user.valid?
+  end
+
+  test "lastname should be present" do
+    @user.lastName = " "
+    assert_not @user.valid?
+  end
+
+  test "email should be present" do
+    @user.email = " "
+    assert_not @user.valid?
+  end
+
+  test "firstName should not be too long" do
+    @user.firstName = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "lastName should not be too long" do
+    @user.lastName = "a" * 51
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.co a_us-ER@foo.bar.eu first.last@foo.jp alice+bob@eve.cn]
+    valid_addresses.each do |address|
+      @user.email = address
+      assert @user.valid? "#{address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[foo@bar..com user@example,com user_foo.org user@example. foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |address|
+      @user.email = address
+      assert_not @user.valid?, "#{address.inspect} should be invalid"
+    end
+  end
+
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
 end

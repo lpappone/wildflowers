76ddc85beba18bf22f09f6bdb2e867b667f4f46b
Lauren Pappone <lauren.pappone@optimizely.com>
2016-10-16 15:55:51 -0700

    finish signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # Handle successful save
+      flash[:success] = "Welcome to Wildflower!"
+      redirect_to @user #equivalent to redirect_to user_url(@user)
     else
       render 'new'
     end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -5,7 +5,8 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     get signup_path
     assert_select 'form[action="/signup"]'
     assert_no_difference 'User.count' do
-      post users_path, params: { user: {name: "",
+      post users_path, params: { user: { firstName: "",
+                                 lastName: "",
                                  email: "user@invalid",
                                  password: "monkey",
                                  password_confirmation: "turtle" } }
@@ -13,5 +14,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
+  end
+
+  test "valid signup submission" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { firstName: "FakeUser",
+                                 lastName: "HasALastName",
+                                 email: "user@valid.true",
+                                 password: "monkey",
+                                 password_confirmation: "monkey" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_equal 'Welcome to Wildflower!', flash[:success]
   end
 end

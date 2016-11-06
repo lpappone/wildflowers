29a373cbdd3d94a42ad6ff56fa1e2e15b7a71860
Lauren Pappone <lauren.pappone@optimizely.com>
2016-11-06 15:13:08 -0800

    implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to Wildflower!"
       redirect_to @user #equivalent to redirect_to user_url(@user)
     else
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -28,5 +28,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
     assert_equal 'Welcome to Wildflower!', flash[:success]
+    assert is_logged_in?
   end
 end
